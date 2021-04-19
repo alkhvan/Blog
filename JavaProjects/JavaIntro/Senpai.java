7648b4813c3d7ca6f66015c101250a3d33c073d3
Aleksandra Khvan <Aleksandra.Khvan@t-systems.com>
2021-04-19 06:12:57 +0000

    Senpai.java was added

---
+++ JavaProjects/JavaIntro/Senpai.java
@@ -0,0 +1,14 @@
+package com.wildcodeschool.JavaProjects.NoticeMe;
+
+public class Senpai {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a = 56;
+int b = 78;
+int result =b-a;
+System.out.println(result);
+	}
+
+}
+
